09dad62c8f4ab128edb23bc4aead6574b097d94f
Md Taifur <taifurislamashraf@gmail.com>
2025-01-28 21:37:04 +0600

    fix: (frontend) auth type

---
+++ apps/frontend/types/user.d.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  fullName: string;
+  email: string;
+  isSocialAuth: boolean;
+  avatar?: string;
+  role: "admin" | "user";
+  address?: string;
+  phone?: string;
+  reviewsInfo?: {
+    productId: string;
+    reviewsCounter?: number;
+  }[];
+}
