fe510409c54b1f966e926306f948c4d7126fbd40
TaifurIslamAshraf <Taifurislamashraf@gmail.com>
2025-06-14 13:41:51 +0600

    fix: category

--- apps/frontend/components/CategoryFilters.tsx
+++ apps/frontend/components/CategoryFilters.tsx
@@ -20,7 +20,7 @@ const CategoryFilters = ({ categories }: Props) => {
         {categories?.map((item) => (
           <Link
             className="py-1.5 px-2 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors"
-            href={`/category/${item._id}/ctegory`}
+            href={`/category/${item._id}/category`}
             key={item._id}
           >
             {item.name}
