f75c2560a06294fd70fb5bd1d00615093e7fb321
md taifur <taifurislamashraf@gmail.com>
2025-11-19 17:52:07 +0600

    fix: gtm

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -17,9 +17,17 @@ const nextConfig = {
         protocol: "https",
         hostname: "server.prakrito.com",
         
-      }
+      } 
     ]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/js/:path*',
+        destination: 'https://serverstape.prakrito.com/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
